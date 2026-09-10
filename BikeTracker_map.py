38beaeac4fad37674159f91116049490dbc2c893
ctrl-markovmaks <markov.maks.247@gmail.com>
2026-09-10 20:32:40 +0300

    Update BikeTracker_map.py

--- BikeTracker_map.py
+++ BikeTracker_map.py
@@ -114,7 +114,7 @@ with tab_map:
 
         hours = st.sidebar.slider("Часы суток", 0, 23, (0, 23))
 
-        search_radius = 7000
+        search_radius = 6000
 
 # Проверяем, выбран ли режим интенсивности для проезда
         is_parking = "парк" in str(event).lower()
